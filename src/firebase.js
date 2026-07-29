1591a430231273272f394dd6b885ed17f20ea40b
Raj <ratanraj9777@gmai.com>
2026-07-29 11:49:55 +0530

    Configure live Firebase credentials for Firestore real-time sync

--- src/firebase.js
+++ src/firebase.js
@@ -14,13 +14,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  apiKey: "AIzaSyBMbD4hMY1yquqP0vgIHbZnm4i5D-6ms1w",
+  authDomain: "manodaya.firebaseapp.com",
+  projectId: "manodaya",
+  storageBucket: "manodaya.firebasestorage.app",
+  messagingSenderId: "402526689305",
+  appId: "1:402526689305:web:89c5e574be203d29057288",
+  measurementId: "G-19PBX68460"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
